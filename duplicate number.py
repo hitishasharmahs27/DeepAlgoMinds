9d230f87d7593881a719371c90b86de68ecec37a
hitishasharmahs27 <hitishasharmahs27@gmail.com>
2026-06-03 22:05:47 +0530

    Add files via upload

---
+++ duplicate number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findDuplicate(self, nums):
+        # Phase 1: Find intersection point
+        slow = nums[0]
+        fast = nums[0]
+
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+            if slow == fast:
+                break
+
+        # Phase 2: Find entrance of cycle
+        slow = nums[0]
+
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+
+        return slow
